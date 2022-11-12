cc0d686065b7dd958aa1fee0f4e6cad5842be471
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2022-11-13 01:05:41 +0200

    Add project files.

---
+++ Two Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = new int[] {2, 3, 4, 5, 6};
+
+            int[] result = TwoSum(ints, 10);
+
+            Console.WriteLine($"[{result[0]}, {result[1]}]");
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            int[] arr = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int Value = target - nums[i];
+
+                if (dict.ContainsKey(Value) && i != dict[Value])
+                {
+                    arr[0] = dict[Value];
+                    arr[1] = i;
+                    return arr;
+                }
+                dict[nums[i]] = i;
+            }
+            return arr;
+        }
+    }
+}
